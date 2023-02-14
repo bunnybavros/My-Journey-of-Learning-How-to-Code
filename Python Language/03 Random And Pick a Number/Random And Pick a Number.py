7036cc3c3b57c5f07cc35d91c6632f49a85a5d79
Bavros <67104562+bunnybavros@users.noreply.github.com>
2023-02-14 16:01:35 +0700

    Create Random And Pick a Number.py
    just add .py at the end of the file

---
+++ Python Language/03 Random And Pick a Number/Random And Pick a Number.py
@@ -0,0 +1,10 @@
+#A program where it would random you a number
+
+#Import Random Library, I guess?
+import random
+
+#Random and pick a number
+Number = random.randint(0,100)
+
+#Print the number that picked
+print(Number)
